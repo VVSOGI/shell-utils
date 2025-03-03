61c84be26fcd0ab8a4529859172eee4594ce3041
benny <vvsogi@gmail.com>
2025-03-03 21:30:20 +0900

    Refactor label synchronization script

--- github-label-synchronize/github-label-sync.sh
+++ github-label-synchronize/github-label-sync.sh
@@ -20,8 +20,6 @@ fi
 
 CURRENT_LABELS=$(curl -s -H "Authorization: token $ACCESS_TOKEN" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$REPOSITORY/labels")
 
-echo $CURRENT_LABELS
-
 if [[ "$CURRENT_LABELS" == *"Not Found"* ]]; then
     exit 1
 fi
@@ -32,3 +30,40 @@ fi
 
 
 CURRENT_LABEL_COUNT=$(echo "$CURRENT_LABELS" | jq '. | length')
+NEW_LABELS=$(cat "$LABELS_FILE")
+NEW_LABEL_COUNT=$(echo "$NEW_LABELS" | jq '. | length')
+
+for row in $(echo "${NEW_LABELS}" | jq -r '.[] | @base64'); do
+    LABEL=$(echo "${row}" | base64 --decode)    
+    NAME=$(echo "${LABEL}" | jq -r '.name')
+    COLOR=$(echo "${LABEL}" | jq -r '.color')
+    DESCRIPTION=$(echo "${LABEL}" | jq -r '.description // ""')
+    EXISTING_LABEL=$(echo "${CURRENT_LABELS}" | jq -r --arg name "$NAME" '.[] | select(.name == $name)')
+
+    if [ -n "$EXISTING_LABEL" ]; then
+        echo -e "update:$NAME"
+        
+        if [ "$DRY_RUN" = false ]; then
+            ENCODED_NAME=$(echo "$NAME" | sed 's/ /%20/g' | sed 's/#/%23/g')
+            
+            curl -s -X PATCH \
+                -H "Authorization: token $ACCESS_TOKEN" \
+                -H "Accept: application/vnd.github.v3+json" \
+                -d "{\"name\":\"$NAME\",\"color\":\"$COLOR\",\"description\":\"$DESCRIPTION\"}" \
+                "https://api.github.com/repos/$REPOSITORY/labels/$ENCODED_NAME" > /dev/null
+        fi
+    else
+        echo "create:$NAME"
+        
+        if [ "$DRY_RUN" = false ]; then
+            curl -s -X POST \
+                -H "Authorization: token $ACCESS_TOKEN" \
+                -H "Accept: application/vnd.github.v3+json" \
+                -d "{\"name\":\"$NAME\",\"color\":\"$COLOR\",\"description\":\"$DESCRIPTION\"}" \
+                "https://api.github.com/repos/$REPOSITORY/labels" > /dev/null
+        fi
+    fi
+done
+
+echo "done"
+exit 0
